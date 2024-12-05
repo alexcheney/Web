7af00915edfaf83cc1f93edeee4112c3c5717cde
cheney <alexcheney@163.com>
2024-12-05 22:14:50 +0800

    test react js

--- Learning/ReactJS/cainiao/my-first-react-app/src/index.js
+++ Learning/ReactJS/cainiao/my-first-react-app/src/index.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />  
-  </React.StrictMode>
+const items = [
+  "2 lb salmon",
+  "3 blost lemons"
+]
+
+var elementlst = items.map((item) => {
+  return React.createElement("li", {}, item)
+})
+
+const numbers = [1,2,3,4,5]
+const res = numbers.reduce((acc, curr) => {
+  return acc + curr
+})
+
+console.log(res)
+
+console.log(elementlst)
+
+var element = React.createElement(
+  "ul",
+  {className:"ingredients"},
+  elementlst
 );
+
+root.render(element, document.getElementById('root'))
+
+// root.render(
+//   <React.StrictMode>
+//     <App />  
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
